098bbf0dfc2810687e9e92af9f4699ed59569466
Nelio Alves <acenelio@gmail.com>
2019-08-08 22:08:02 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
